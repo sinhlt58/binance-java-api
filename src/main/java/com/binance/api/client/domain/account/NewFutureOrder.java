fcb4be2aa9f122d3880bec95c45f89d0d669e005
Luu Truong Sinh <sinhlt58@gmail.com>
2022-08-23 23:33:39 +0700

    change to limit order

--- src/main/java/com/binance/api/client/domain/account/NewFutureOrder.java
+++ src/main/java/com/binance/api/client/domain/account/NewFutureOrder.java
@@ -37,7 +37,7 @@ public class NewFutureOrder {
 
     public NewFutureOrder(String symbol, boolean isForBatch){
         this.symbol = symbol;
-        this.timeInForce = TimeInForce.GTX; // post only order
+        this.timeInForce = TimeInForce.GTC; // post only order
         this.newOrderRespType = NewOrderResponseType.RESULT;
         if (!isForBatch){
             this.timestamp = System.currentTimeMillis();
