9dccd3b28789c6dd2c6d5ae8a736d67404037e72
Luu Truong Sinh <sinhlt58@gmail.com>
2024-03-05 21:45:59 +0700

    update

--- src/main/java/com/binance/api/client/domain/event/future/FutureEventOrderTradeUpdate.java
+++ src/main/java/com/binance/api/client/domain/event/future/FutureEventOrderTradeUpdate.java
@@ -36,15 +36,15 @@ public class FutureEventOrderTradeUpdate {
 
     @JsonProperty("rp")
     String realizedProfit;
-    
+
     @JsonProperty("l")
-    private String quantityLastFilledTrade;
+    String quantityLastFilledTrade;
     @JsonProperty("z")
-    private String accumulatedQuantity;
+    String accumulatedQuantity;
     @JsonProperty("L")
-    private String priceOfLastFilledTrade;
+    String priceOfLastFilledTrade;
     @JsonProperty("n")
-    private String commission;
+    String commission;
 
     @Override
     public String toString() {
